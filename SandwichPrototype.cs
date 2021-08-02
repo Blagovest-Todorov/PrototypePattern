dc0eea3b1f4de5e802bf4a3897c9db1e848e3c0a
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-08-02 11:12:04 +0300

    init the Project files

---
+++ SandwichPrototype.cs
@@ -0,0 +1,14 @@
+﻿namespace PrototypePattern
+{
+    public abstract class SandwichPrototype
+    {
+        public abstract SandwichPrototype Clone();
+
+            // Prototype –pattern that gives us possibility to clone Objects.
+            //Clone method to clone obejcts to create exact copie of a given object.
+            //Design Paters are , easy to support, to ease the code readability, better support.
+            //Reusability////code.
+            //Design Patterns lead to Highquality Code !  this is the idea.
+
+    }
+}
---
+++ Sandwitch.cs
@@ -0,0 +1,30 @@
+﻿namespace PrototypePattern
+{
+    using System;
+    public class Sandwitch : SandwichPrototype
+    {
+        private string bread;
+        private string meat;
+        private string cheese;
+        private string veggies;
+
+        public Sandwitch(string bread, string meat, string cheese, string veggies)
+        {
+            this.bread = bread;
+            this.meat = meat;
+            this.cheese = cheese;
+            this.veggies = veggies;
+        }
+        public override SandwichPrototype Clone()
+        {
+            string ingridientsList = this.GetIngridientsList();
+            Console.WriteLine($"Cloning sandwich with ingridients:{ingridientsList}");
+            return this.MemberwiseClone() as SandwichPrototype;
+        }
+
+        private string GetIngridientsList() 
+        {
+            return $"{this.bread}, {this.meat}, {this.cheese}, {this.veggies}"; 
+        }
+    }
+}
